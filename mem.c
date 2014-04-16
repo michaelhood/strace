23f01cc31b538a270b24bbbd32b64d0da13de220
Masatake YAMATO <yamato@redhat.com>
2014-04-16 15:33:35 +0900

    unwind: move stacktrace capturing and mmap cache invalidating to trace_syscall_entering
    Instead of handling stacktrace capturing and mmap cache invalidating in
    sys_* functions, handle them uniformly in trace_syscall_entering using
    new flags introduced by previous two commits.
    
    The patch is simpler than its older version(v3).  The value of
    hide_log_until_execve is just ignored.  I found the value is nothing
    to do with this patch.  unwind_cache_invalidate is mentioned only
    once in trace_syscall_exiting.
    Both are suggested by Dmitry Levin.
    
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

--- mem.c
+++ mem.c
@@ -172,13 +172,6 @@ static int
 print_mmap(struct tcb *tcp, long *u_arg, unsigned long long offset)
 {
 	if (entering(tcp)) {
-#ifdef USE_LIBUNWIND
-		if (stack_trace_enabled) {
-			unwind_capture_stacktrace(tcp);
-			unwind_cache_invalidate(tcp);
-		}
-#endif
-
 		/* addr */
 		if (!u_arg[0])
 			tprints("NULL, ");
@@ -307,17 +300,7 @@ sys_munmap(struct tcb *tcp)
 	if (entering(tcp)) {
 		tprintf("%#lx, %lu",
 			tcp->u_arg[0], tcp->u_arg[1]);
-#ifdef USE_LIBUNWIND
-		if (stack_trace_enabled)
-			unwind_capture_stacktrace(tcp);
-#endif
 	}
-#ifdef USE_LIBUNWIND
-	else {
-		if (stack_trace_enabled)
-			unwind_cache_invalidate(tcp);
-	}
-#endif
 	return 0;
 }
 
@@ -328,17 +311,7 @@ sys_mprotect(struct tcb *tcp)
 		tprintf("%#lx, %lu, ",
 			tcp->u_arg[0], tcp->u_arg[1]);
 		printflags(mmap_prot, tcp->u_arg[2], "PROT_???");
-#ifdef USE_LIBUNWIND
-		if (stack_trace_enabled)
-			unwind_capture_stacktrace(tcp);
-#endif
 	}
-#ifdef USE_LIBUNWIND
-	else {
-		if (stack_trace_enabled)
-			unwind_cache_invalidate(tcp);
-	}
-#endif
 	return 0;
 }
 
